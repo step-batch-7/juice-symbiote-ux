6a05a346ae34335defc218c8652abbddc3a2bf19
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-11-26 11:57:19 +0530

    mock test for read,write,file exist completed
    it covers all statement, function,branches
    one new library file has been added that includes read and write
    function
    No changes made for the updation of the program according to library
    file
    program only works for the valid user args in specified order

---
+++ src/library.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const assert = require("assert");
+
+const readFile = function(path, encoder, reader, isExist) {
+  if (!isExist(path)) {
+    return {};
+  }
+  return reader(path, encoder);
+};
+
+const writeFile = function(path, content, encoder, writer) {
+  return writer(path, content, encoder);
+};
+
+exports.readFile = readFile;
+exports.writeFile = writeFile;
---
+++ test/testLibrary.js
@@ -0,0 +1,52 @@
+const assert = require("assert");
+const lib = require("../src/library.js");
+const readFile = lib.readFile;
+const writeFile = lib.writeFile;
+
+//------------------------------------------------------------testing read file -------------
+describe("readFile", function() {
+  it("it should check path given to isExist is correct, if path doesn't exist return empty object", function() {
+    const isExists = function(path) {
+      assert.strictEqual(path, "correctPath");
+      return false;
+    };
+    const reader = function() {
+      return "file";
+    };
+    assert.deepStrictEqual(
+      readFile("correctPath", "utf8", reader, isExists),
+      {}
+    );
+  });
+  it("when path exist,it should return content of file ", function() {
+    const isExists = function(path) {
+      return true;
+    };
+    const reader = function(path, encoder) {
+      assert.strictEqual(path, "correctPath");
+      assert.strictEqual(encoder, "utf8");
+      return "ckecked the reader";
+    };
+    assert.deepStrictEqual(
+      readFile("correctPath", "utf8", reader, isExists),
+      "ckecked the reader"
+    );
+  });
+});
+
+//--------------------------test write -----------------------
+
+describe("writeFile", function() {
+  it("should check for the correct path, encoder and should return content of the file", function() {
+    const writer = function(path, content, encoder) {
+      assert.strictEqual(path, "correctPath");
+      assert.strictEqual(content, "details of the file");
+      assert.strictEqual(encoder, "utf8");
+      return "details of the file";
+    };
+    assert.deepStrictEqual(
+      writeFile("correctPath", "details of the file", "utf8", writer),
+      "details of the file"
+    );
+  });
+});
